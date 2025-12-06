9330ead9acc11344d8c2a6ca99b4865daae106f0
watzal <vatsallakhmani@gmail.com>
2025-12-06 12:29:50 +0530

    changes made

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ st.title("Medical Chatbot")
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 GEMINI_API_KEY=os.environ.get('GEMINI_API_KEY')
 
-# os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-# os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
 
 if not PINECONE_API_KEY:
     st.error("Missing PINECONE_API_KEY in Streamlit Secrets")
